6828c53d3a7b5af1e79ed4b65c171e8f4fd68095
Sohil Habib <vsohail09@gmail.com>
2013-11-30 14:57:01 -0500

    Clienterror complete

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -44,8 +44,22 @@ void pipe_handler(int sig)
 void clienterror(int fd,char *cause,char *errnum,char *shortmsg.char *longmsg)
 {
   char buf[MAXLINE], body[MAXBUF];
-  /* Build the HTTP response body */
 
+  /* Build the HTTP response body */
+  sprintf(body, "<html><title>Proxy Error</title>");
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Proxy server</em>\r\n", body);
+
+  /* Print the HTTP response */
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
 }
 void *my_proxy(void *fd)
 {
