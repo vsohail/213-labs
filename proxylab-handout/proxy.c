8daeabee210f0e5b63bf0e75e4f40bd22840f044
vsohail <vsohail09@gmail.com>
2013-11-30 14:43:00 -0500

    Main Function

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "csapp.h"
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
@@ -8,10 +8,40 @@
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
 static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
+void pipe_handler(int sig);
+void *my_proxy(void *fd);
 
-int main()
+int main(int argc,char **argv)
 {
-    printf("%s%s%s", user_agent_hdr, accept_hdr, accept_encoding_hdr);
+  user_agent_hdr=0;
+  accept_hdr=0;
+  accept_encoding_hdr=0;
+  int listenfd,port;
+  int *connfd;
+  socklen_t client_len;
+  struct sockaddr_in client_addr;
+  pthread_t tid;
 
-    return 0;
+  if(argc != 2) {
+    fprintf(stderr,"usage: %s <port>\n",argv[0]);
+    exit(1);
+  }
+  Signal(SIGPIPE,pipe_handler);
+  port = atoi(argv[1]);
+  listenfd = Open_listenfd(port);
+  client_len = sizeof(struct sockaddr_in);
+  while(1) {
+    connfd = Malloc(sizeof(int));
+    *connfd = Accept(listenfd,(SA *)&client_addr,&client_len);
+    Pthread_create(&tid,NULL,my_proxy,connfd);
+  }
+  return 0;
+}
+void pipe_handler(int sig)
+{
+  return;
+}
+void *my_proxy(void *fd)
+{
+  return NULL;
 }
