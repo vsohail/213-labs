ce84581a9f35797bd246bbcaeced3cb7f6af8645
vsohail <vsohail09@gmail.com>
2013-12-07 23:09:50 -0500

    Last Commit: Chalk Dinosaur - Sediment

--- proxylab-handout/cache.c
+++ proxylab-handout/cache.c
@@ -1,3 +1,23 @@
+/*
+ * Name: Sohil Habib
+ * Andrew Id:snhabib
+ *
+ * LRU cache
+ * The cache follows the readers-writers sollution favouring
+ * multiple readers
+ * The cache structhas
+ * ->the size,
+ * ->url for matching as a hit,
+ * ->the pointer to the data
+ * ->and the freshness value obtained using the C time function
+ * Each block points to the next block
+ *
+ * The cache inserts an element with a full write lock
+ * and evicts least fresh elements
+ *
+ * The retreive function does not read a write lock
+ * as it performs an atomic cache freshness update
+ */
 #include "cache.h"
 
 int cur_size;
@@ -8,6 +28,7 @@ static struct cache *head;
 static sem_t mutex;
 static sem_t w;
 static int readcount;
+/* Initialize the cache */
 void cache_init(int max_cache_size,int max_obj_size)
 {
   max_size=max_cache_size;
@@ -15,16 +36,22 @@ void cache_init(int max_cache_size,int max_obj_size)
   init_block = Malloc(sizeof(struct cache));
   head=init_block;
   init_block->next=NULL;
+  /* Initialize the mutexes */
   Sem_init(&mutex, 0, 1);
   Sem_init(&w, 0, 1);
   readcount=0;
 }
+/*
+ * Insert a block into the cache
+ */
 void insert(char *url,void *data,int size)
 {
+  /* if cache insertion exceeds MAX_CACHE_SIZE, return */
   if(size>max_size)
     return;
-  P(&w);
+  P(&w); /* Obtain write lock */
   struct cache *new_entry = Malloc(sizeof(struct cache));
+  /* Initialize the new block */
   new_entry->size = size;
   cur_size += size;
   new_entry->data = data;
@@ -33,6 +60,7 @@ void insert(char *url,void *data,int size)
   new_entry->freshness = (long)time(0);
   head->next = new_entry;
   head = new_entry;
+  /* Initialize variables needed for traversal */
   struct cache *move = init_block->next;
   long tmp = move->freshness;
   struct cache *lru=move,*prev=init_block;
@@ -50,16 +78,22 @@ void insert(char *url,void *data,int size)
       }
       move=move->next;
     }
+    /* Evict least fresh(LRU) element from cache */
     prev->next=lru->next;
     cur_size-=lru->size;
     Free(lru->data);
     Free(lru);
     lru = NULL;
   }
-  V(&w);
+  V(&w); /* Release write lock */
 }
+/*
+ * Reads the cache to find a hit
+ * Updates freshness if accessed
+ */
 void *retreive(char *url,int *size)
 {
+  /* Update readcount and prevent write */
   P(&mutex);
   readcount++;
   if(readcount==1)
@@ -70,13 +104,14 @@ void *retreive(char *url,int *size)
   void *data = NULL;
   while(cur!=NULL) {
     if(!strcmp(cur->url,url)) {
-      cur->freshness = (long)time(0);
+      cur->freshness = (long)time(0); /* update freshness */
       *size = cur->size;
       data = cur->data;
       break;
     }
     cur=cur->next;
   }
+  /* decrement readcount and release writelock if no more readers */
   P(&mutex);
   readcount--;
   if(readcount==0)
--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -1,3 +1,13 @@
+/*
+ * Name: Sohil Habib
+ * Andrew Id:snhabib
+ *
+ * A simple concurrent proxy server
+ * for communication between a client and a server.
+ * Handles GET requests using HTTP 1.0
+ * The proxy has a cache which caches blocks of
+ * MAX_OBJECT_SIZE upto a maximum of MAX_CACHE_SIZE
+ */
 #include <stdio.h>
 #include "cache.h"
 #include "csapp.h"
@@ -14,8 +24,12 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
     char *longmsg);
 int parse(char *uri, char *host, char *path);
 int open_clientfd_r(char *hostname, char *port);
-int Open_clientfd_r(char *hostname, char* port);
-
+/*
+ * The main routine which keeps
+ * opening sockets to service client requests
+ * and creates threads per request to make the
+ * server concurrent.
+ */
 int main(int argc,char **argv)
 {
   int listenfd,port,*connfd;
@@ -27,53 +41,66 @@ int main(int argc,char **argv)
     fprintf(stderr,"usage: %s <port>\n",argv[0]);
     exit(1);
   }
-  Signal(SIGPIPE,SIG_IGN);
+  Signal(SIGPIPE,SIG_IGN); /* Disable SIGPIPE */
   port = atoi(argv[1]);
-  cache_init(MAX_CACHE_SIZE,MAX_OBJECT_SIZE);
+  cache_init(MAX_CACHE_SIZE,MAX_OBJECT_SIZE); /* Initialize Cache */
   listenfd = Open_listenfd(port);
   client_len = sizeof(struct sockaddr_in);
   while(1) {
     connfd = Malloc(sizeof(int));
     *connfd = Accept(listenfd,(SA *)&client_addr,&client_len);
-    Pthread_create(&tid,NULL,my_proxy,connfd);
+    Pthread_create(&tid,NULL,my_proxy,connfd); /* Create a new thread */
   }
   return 0;
 }
+/*
+ * Parse the url to obtain
+ * host, port number and path
+ * ->if uri is invalid, return
+ * ->if no path is specified,
+ *   append a '/'
+ * ->if no port is specified
+ *   default to 80(HTTP)
+ * return type: int port
+ */
 int parse(char *uri, char *host, char *path)
 {
-  char *p, *q;
+  char *p, *q; /* dummy variables to traverse the url */
   int port;
   p = strstr(uri, "://");
   if (!p) {
     return -1;
   }
 
-  p += 3;
-  q = strpbrk(p, " :/");
+  p += 3; /* progress to end of '://' */
+  q = strpbrk(p, ":/"); /* Detect a :,/ */
 
-  strncpy(host, p, q-p);
-  host[q-p] = '\0';
+  strncpy(host, p, q-p); /* get the host */
+  host[q-p] = '\0'; /* terminate host with NULL */
 
   if (*q == ':') {
     q++;
-    port = atoi(q);
+    port = atoi(q); /* set port */
   }
   else
-    port = HTTP;
+    port = HTTP; /* else default to 80 (HTTP) */
 
   q = strstr(p, "/");
   if (!q) {
     path = "/";
     return port;
   }
-  strncpy(path, q, strlen(q));
+  strncpy(path, q, strlen(q)); /* update path is specified */
   path[strlen(q)] = '\0';
 
   return port;
 
 }
-
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+/*
+ * Error handling function
+ */
+void clienterror(int fd, char *cause, char *errnum,
+    char *shortmsg, char *longmsg)
 {
   char buf[MAXLINE], body[MAXBUF];
 
@@ -93,33 +120,45 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
   rio_writen(fd, buf, strlen(buf));
   rio_writen(fd, body, strlen(body));
 }
+/*
+ * Main proxt threat function
+ * Gets the fd, communictaes with server,
+ * redirects requests to client, kills threat
+ * and handles all the other errors related to
+ * the proxy. Also calls cache insert and retreive
+ * functions.
+ */
 void *my_proxy(void *fd_proxy)
 {
   int clientfd, fd,port;
   char port_str[10];
   size_t n;
   int size;
-  char response[MAXBUF], req[MAXBUF];
-  char buf[MAXLINE],host[MAXLINE],path[MAXLINE],url[MAXLINE], version[MAXLINE], method[MAXLINE];
+  char response[MAXBUF],req[MAXBUF];
+  char buf[MAXLINE],host[MAXLINE];
+  char path[MAXLINE],url[MAXLINE];
+  char version[MAXLINE],method[MAXLINE];
   rio_t rio,rio_c;
   void *to_cache;
-  Pthread_detach(pthread_self()); 
+  Pthread_detach(pthread_self()); /* detatch thread */
   fd = *((int *)fd_proxy);
   Free(fd_proxy);
   rio_readinitb(&rio, fd);
   rio_readlineb(&rio, buf, MAXLINE);
+  /* Parse the method,url and version */
   sscanf(buf, "%s %s %s", method, url, version);
-  if (strcasecmp(method, "GET")) {
+  if (strcasecmp(method, "GET")) { /* throw error for Non-GET requests */
     clienterror(fd, method, "501", "Method Not Implemented",
         "This request is not currently supported");
     Close(fd);
     return NULL;
   }
-  if ((port=parse(url, host, path)) != -1) {
+  if ((port=parse(url, host, path)) != -1) { /* If successfully parsed */
     if((to_cache=retreive(url,&size))!=NULL) {
-      rio_writen(fd,to_cache,size);
+      rio_writen(fd,to_cache,size); /* If present in cache */
     }
     else {
+      /* The main request consisting of method,path,version */
       sprintf(req, "%s %s HTTP/1.0\r\n", method,path);
       while(strcmp(buf, "\r\n")) {
         rio_readlineb(&rio, buf, MAXLINE);
@@ -127,20 +166,21 @@ void *my_proxy(void *fd_proxy)
             && !strstr(buf, "User-Agent:")
             && !strstr(buf, "Accept:")
             && !strstr(buf, "Accept-Encoding:")
-            && !strstr(buf, "Connection:") 
+            && !strstr(buf, "Connection:")
             && !strstr(buf, "Proxy-Connection:")) {
-          strcat(req, buf);
+          strcat(req, buf); /* Form the request leaving out the above */
         }
       }
-      if (!strstr(req, "Host:")) {
+      if (!strstr(req, "Host:")) { /* Enter host */
         sprintf(req, "%sHost: %s\r\n",req,host);
       }
+      /* Update all the headers ignored above */
       sprintf(req, "%s%s",req,user_agent_hdr);
       sprintf(req, "%s%s",req,accept_hdr);
       sprintf(req, "%s%s",req,accept_encoding_hdr);
       sprintf(req, "%sConnection: close\r\n",req);
       sprintf(req, "%sProxy-Connection: close\r\n",req);
-      sprintf(req, "%s\r\n",req);
+      sprintf(req, "%s\r\n",req); /* Terminating the header */
       sprintf(port_str,"%d",port);
       clientfd = open_clientfd_r(host,port_str);
 
@@ -159,6 +199,7 @@ void *my_proxy(void *fd_proxy)
         }
         size+=n;
         if(size<=MAX_OBJECT_SIZE) {
+          /* cache till block less than MAX_OBJECT_SIZE */
           to_cache = Realloc(to_cache,size);
           memcpy(to_cache + (size-n),response,n);
         }
@@ -168,6 +209,7 @@ void *my_proxy(void *fd_proxy)
         rio_writen(fd, response, n);
       }
       if(to_cache) {
+        /* if cachable then insert in cache */
         insert(url,to_cache,size);
       }
       Close(clientfd);
@@ -180,6 +222,9 @@ void *my_proxy(void *fd_proxy)
   Close(fd);
   return NULL;
 }
+/*
+ * Threadsafe open_clientfd function (Thanksgiving Gift)
+ */
 int open_clientfd_r(char *hostname, char *port) {
   int clientfd;
   struct addrinfo *addlist, *p;
